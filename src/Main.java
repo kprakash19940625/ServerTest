28abb7845cd348df8f3165561a13b23753b4e0c3
Prakash K <kprakash19940625@gmail.com>
2016-01-21 16:07:26 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class Main {
-  public static void main(String args[]) {
-    
-  }
+
+	@Test
+	public void test1() {
+		GenerateRandom test = new GenerateRandom();
+		int output = test.generate();
+		assertEquals(25,output);
+	}
+	@Test
+	public void test2(){
+		GenerateRandom test = new GenerateRandom();
+		int output = test.generate();
+		assertEquals(36,output);
+	}
+	@Test
+	public void test3(){
+		GenerateRandom test = new GenerateRandom();
+		int output = test.generate();
+		assertEquals(49,output);
+	}
+
 }
