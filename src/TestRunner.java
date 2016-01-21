8baaead29f7cc5cfdd30f8c6b019cd75f0e5da72
Prakash K <kprakash19940625@gmail.com>
2016-01-21 17:34:24 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -5,9 +5,6 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(Main.class);
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
       System.out.println(result.wasSuccessful());
    }
 }
