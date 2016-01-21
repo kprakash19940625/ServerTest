a88e1f92b4adaf9423dac7edf2a387fb6a4976cd
Prakash K <kprakash19940625@gmail.com>
2016-01-21 20:12:07 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -5,6 +5,8 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(Main.class);
-      System.out.println(result.wasSuccessful());
+      System.out.println("Total Test Cases Runned : "+getRunCount());
+      System.out.println("Total Test Cases Failed : "+getFailureCount());
+      System.out.println("Total Test Cases Successful : "+(getRunCount()-getFailureCount()));
    }
 }
