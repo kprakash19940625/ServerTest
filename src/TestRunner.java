b2766ce88004ba280630217561377268069c9bc1
Prakash K <kprakash19940625@gmail.com>
2016-01-22 17:21:42 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -4,12 +4,25 @@ import org.junit.runner.notification.Failure;
 import java.util.EmptyStackException;
 import java.io.*;
 
-public class TestRunner throw FileNotFoundException {
+public class TestRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(Main.class);
       int total = result.getRunCount();
       int failed = result.getFailureCount();
       int succeeded = total - failed;
+      try {
+         PrintWriter pw = new PrintWriter("Report.txt");
+         pw.println("Total Test Cases Runned : "+total);
+         pw.println("Total Test Cases Failed : "+failed);
+         pw.println("Total Test Cases Successful : "+succeeded);
+         if (failed>0) {
+            pw.println("Failed Cases : \n\t "+result.getFailures());
+         }
+         pw.flush();
+        }
+      catch (Exception ex) {
+         ex.printStackTrace();
+      }
       /*
       System.out.println("Total Test Cases Runned : "+total);
       System.out.println("Total Test Cases Failed : "+failed);
@@ -17,14 +30,6 @@ public class TestRunner throw FileNotFoundException {
       if (failed>0) {
          System.out.println("Failed Cases : \n\t "+result.getFailures());
       }*/
-      PrintWriter writer = new PrintWriter("Report.txt", "UTF-8");
-      writer.println("Total Test Cases Runned : "+total);
-      writer.println("Total Test Cases Failed : "+failed);
-      writer.println("Total Test Cases Successful : "+succeeded);
-      if (failed>0) {
-         writer.println("Failed Cases : \n\t "+result.getFailures());
-      }
-      writer.close();
       if (failed>2) {
         throw new EmptyStackException();
       }
