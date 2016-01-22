5fc14e166c0f943e53eb6d912758031cdf731b83
Prakash K <kprakash19940625@gmail.com>
2016-01-22 16:50:08 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -2,6 +2,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import java.util.EmptyStackException;
+import java.io.*;
 
 public class TestRunner {
    public static void main(String[] args) {
@@ -9,12 +10,21 @@ public class TestRunner {
       int total = result.getRunCount();
       int failed = result.getFailureCount();
       int succeeded = total - failed;
+      /*
       System.out.println("Total Test Cases Runned : "+total);
       System.out.println("Total Test Cases Failed : "+failed);
       System.out.println("Total Test Cases Successful : "+succeeded);
       if (failed>0) {
          System.out.println("Failed Cases : \n\t "+result.getFailures());
+      }*/
+      PrintWriter writer = new PrintWriter("Report.txt", "UTF-8");
+      writer.println("Total Test Cases Runned : "+total);
+      writer.println("Total Test Cases Failed : "+failed);
+      writer.println("Total Test Cases Successful : "+succeeded);
+      if (failed>0) {
+         writer.println("Failed Cases : \n\t "+result.getFailures());
       }
+      writer.close();
       if (failed>2) {
         throw new EmptyStackException();
       }
