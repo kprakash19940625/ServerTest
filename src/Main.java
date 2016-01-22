f0c938f04c06603159c23a2d83cfdff0ec84f1fd
Prakash K <kprakash19940625@gmail.com>
2016-01-22 12:38:20 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,27 @@ public class Main {
         public void test2(){
                 GenerateRandom test = new GenerateRandom();
                 int output = test.generate();
-                assertTrue(3 < output);
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test3() {
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test4(){
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
+        }
+        
+        @Test
+        public void test5(){
+                GenerateRandom test = new GenerateRandom();
+                int output = test.generate();
+                assertTrue(5 < output);
         }
 }
